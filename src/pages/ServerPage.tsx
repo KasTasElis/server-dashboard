436cbfbe839080aa06da78d04c111e3e8161fbee
Eli Tamosauskas <tamosauskas09@gmail.com>
2024-02-05 20:49:11 +0200

    progress

--- src/pages/ServerPage.tsx
+++ src/pages/ServerPage.tsx
@@ -2,30 +2,41 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchServers } from "../api";
 import { useToken } from "../context";
 import { useSignOut } from "../hooks";
-import { useSearchParams } from "react-router-dom";
-import { useEffect } from "react";
+import clsx from "clsx";
 
-export const ServerPage = () => {
-  const token = useToken();
+const Header = () => {
   const signOut = useSignOut();
-  let [searchParams, setSearchParams] = useSearchParams();
 
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  //   // The serialize function here would be responsible for
-  //   // creating an object of { key: value } pairs from the
-  //   // fields in the form that make up the query.
-  //   let params = serializeFormQuery(event.target);
-  //   setSearchParams(params);
-  // }
+  return (
+    <>
+      {/* Provides spacer at the top */}
+      <div className="h-16 min-w-full" />
 
-  //console.log({searchParams});
+      <div className="backdrop-blur-sm bg-white/10 shadow-lg fixed top-0 left-0 min-w-full min-h-16">
+        <div className="container flex justify-between items-center mx-auto py-3 px-4">
+          <h1 className="text-xl text-white font-light">🚀 Dashboard</h1>
+          <button
+            onClick={signOut}
+            className="bg-red-500 rounded p-3 py-2 text-white hover:bg-red-400"
+          >
+            👋 Log out
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-  useEffect(() => {
-    console.log('sort changed', searchParams.get('sort'));
+const Message = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="text-center text-white font-light text-xl py-9">
+      <h1>{children}</h1>
+    </div>
+  );
+}
 
-
-  }, [searchParams])
+const ServerList = () => {
+  const token = useToken();
 
   const { isPending, error, data } = useQuery({
     queryKey: ["serverData"],
@@ -33,53 +44,50 @@ export const ServerPage = () => {
     refetchOnWindowFocus: false,
   });
 
+  if (isPending) return <Message><h1 className="animate-pulse">⏳ Loading...</h1></Message>;
+
+  if (error) return <Message><h1>{error.message}</h1></Message>;
+
+  if (data.length === 0) return <Message><h1>🤦‍♂️ Nothing to show...</h1></Message>;
+
   return (
-    <div>
-      <div className="bg-gradient-to-r from-teal-500 to-indigo-500 shadow-lg border-b">
-        <div className="container flex justify-between items-center mx-auto py-3 px-4">
-          <h1 className="text-xl text-slate-200 font-light">
-            Server Dashboard
-          </h1>
-          <button
-            onClick={signOut}
-            className="bg-red-500 rounded p-3 py-2 text-white"
-          >
-            Log out
-          </button>
+    <div className="container mx-auto px-4 py-10">
+      <table className="w-full max-w-xl mx-auto mb-7 rounded-md overflow-hidden shadow-lg">
+        <thead>
+          <tr className="text-left bg-white/70 min-w-full text-slate-700">
+            <th className="px-4 py-3 min-w-full border-r cursor-pointer hover:bg-white/50">Server Name</th>
+            <th className="px-4 py-3 min-w-full cursor-pointer hover:bg-white/50">Distance (km)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map(({ distance, name }, i) => (
+              <tr key={i} className={clsx("border-b border-white/20 bg-white/20 text-slate-100 hover:bg-white/50", {
+                // Add a different background color for every other row
+                "bg-white/40": i % 2 === 0,
 
-          <button onClick={() => {
-            setSearchParams({sort: 'a-z'})
-          }}>Sort A-Z</button>
-          <button onClick={() => {
-            setSearchParams({sort: '1-9'})
-          }}>Sort 1-9</button>
-        </div>
-      </div>
-
-      <div className="mt-5">
-        <div className="container mx-auto px-4">
-          {isPending && <div>Loading...</div>}
-
-          {error && <div>Error: {error.message}</div>}
-
-          <table className="w-full max-w-xl mx-auto mb-7">
-            <thead>
-              <tr className="text-left bg-slate-300 min-w-full">
-                <th className="px-4 py-3 min-w-full border">Server Name</th>
-                <th className="px-4 py-3 min-w-full border">Distance (km)</th>
+              })}>
+                <td key={i} className="border-r px-4 py-1 border-white/40">
+                  {name}
+                </td>
+                <td key={i} className="px-4 py-1">
+                  {distance}
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {data &&
-                data.map(({ name, distance }, i) => (
-                  <tr key={i}>
-                    <td className="border px-4 py-1">{name}</td>
-                    <td className="border px-4 py-1">{distance}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export const ServerPage = () => {
+  return (
+    <div className="bg-gradient-to-r from-teal-500 to-indigo-500 min-h-screen">
+      <Header />
+
+      <div>
+        <ServerList />
       </div>
     </div>
   );
